abf85f311682c99cbc874bbafa90c5d556af06d6
Mohsin121243 <mchoudhary4645@bths.edu>
2023-09-29 23:30:50 -0400

    final comit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Select the element for the entire website
+
   var entireWebsite = document.querySelector(".entire-website");
   
   // Add the CSS class for the animation
   entireWebsite.classList.add("fade-in");
 });
+
